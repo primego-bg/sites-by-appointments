40dfba3b964d35030facaaeab1aeadc39c6aa157
vents04 <vencidim04@gmail.com>
2025-01-14 22:09:43 +0200

    email service init

---
+++ api/services/email.service.js
@@ -0,0 +1,9 @@
+//REMEMBER: app passwords get revoked when Google Account password changes
+
+const EmailService = {
+    sendEmail: async (email, subject, message) => {
+        // send email
+    }
+}
+
+module.exports = EmailService;
